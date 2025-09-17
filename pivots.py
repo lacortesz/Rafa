d9a75d138fde3fd1812181470236c262b39644a3
Luis Alberto Cortes Zambrano <luiscortes@DATECSA.LAN>
2025-09-16 19:53:23 -0500

    clean pivots

--- pivots.py
+++ pivots.py
@@ -10,26 +10,13 @@ trendStrength = 3
 
 #if __name__ == "__main__":
 symbol = '6EZ25.CME'
-start = '2025-07-29'
+start = '2025-07-21'
 end = '2025-07-31'
 interval = '1h'
 data = utils.download_data(symbol, start, end, interval)
 
-#print(data)
-
 pivots = utils.get_pivots(data, pivotStrength, trendStrength)
-print("pivots:", pivots)
 
-#utils.plot(data, symbol)
 utils.plot_candles_with_pivots_mpf(data, pivots, title=f'Candlestick with Pivots {symbol}')
 
-#utils.plot_candles(data, title=f'Candlestick with Pivots {symbol}')
-
-'''pivots = get_pivots(data)
-for pivot in pivots:
-    print(f"Pivot {pivot['type']} at {pivot['index']}: {pivot['value']}")'''
-	
-
-
-#utils.plot_candles_with_pivots(data, pivots, title=f'Candlestick with Pivots {symbol}')
 
--- utils.py
+++ utils.py
@@ -13,26 +13,6 @@ def download_data(ticker, start, end, interval):
         raise ValueError(f"No data fetched for {ticker}. Check ticker or date range.")
     return df
 
-def pivots_(data, pivotStrength, trendStrength):
-    #data = data.copy()
-    #data.index = pd.to_datetime(data.index)
-    pivots = []
-    for i in range(len(data) - trendStrength, trendStrength,-1):
-        high = data['High'].iloc[i]
-        low = data['Low'].iloc[i]
-        print('Counter' + str(i) + ' High: ' + str(high) + ', Low: ' + str(low))
-
-        if high > data['High'].iloc[i-1] and high > data['High'].iloc[i-2] and high > data['High'].iloc[i-3]:
-            #print('Pivot High at index ' + str(data.index[i]) + ' with value ' + str(high))
-            #return [{'type': 'high', 'index': data.index[i], 'value': high}]
-            pivots.append({'type': 'high', 'index': data.index[i], 'value': high})
-        if low < data['Low'].iloc[i-1] and low < data['Low'].iloc[i-2] and low < data['Low'].iloc[i-3]:
-            #print('Pivot Low at index ' + str(data.index[i]) + ' with value ' + str(low))
-            #return [{'type': 'low', 'index': data.index[i], 'value': low}]
-            pivots.append({'type': 'low', 'index': data.index[i], 'value': low})
-
-    return pivots
-
 def get_pivots(data, pivotStrength, trendStrength):
 	pivots = []
 	for i in range(len(data)-pivotStrength, ):
@@ -63,52 +43,34 @@ def get_pivots(data, pivotStrength, trendStrength):
 			pivots.append({'type': 'low', 'index': data.index[i], 'value': low})
 	
     # Devolver los últimos 3 pivots	
-	return pivots
-
+	return clean_pivots(pivots, pivotStrength)
 
 def clean_pivots(pivots, pivotStrength):
-    for i in range(0, len(pivots)-1):
-        if pivots[i+1]['type'] == 'high':
-            if pivots[i]['type'] == 'high':
-                if pivots[i]['value'] > pivots[i+1]['value']:
-                    print('Elimino pivot high ' + str(pivots[i+1]))
-                    pivots.remove(pivots[i+1])
-                else:
+    i = 0
+    while i < len(pivots)-1:
+    #for i in range(0, 42, 1):
+        print('Pivot ' + str(i) + ' de: '  + str(len(pivots)) + ' info ' + str(pivots[i]))
+        if pivots[i]['type'] == 'high':
+            if pivots[i+1]['type'] == 'high':
+                if pivots[i+1]['value'] > pivots[i]['value']:
                     print('Elimino pivot high ' + str(pivots[i]))
                     pivots.remove(pivots[i])
-        if pivots[i+1]['type'] == 'low':
-            if pivots[i]['type'] == 'low':
-                if pivots[i]['value'] < pivots[i+1]['value']:
-                    print('Elimino pivot low ' + str(pivots[i+1]))
-                    pivots.remove(pivots[i+1])
                 else:
+                    print('Elimino pivot high ' + str(pivots[i+1]))
+                    pivots.remove(pivots[i+1])
+                i = 0
+        if pivots[i]['type'] == 'low':
+            if pivots[i+1]['type'] == 'low':
+                if pivots[i+1]['value'] < pivots[i]['value']:
                     print('Elimino pivot low ' + str(pivots[i]))
                     pivots.remove(pivots[i])
+                else:
+                    print('Elimino pivot low ' + str(pivots[i+1]))
+                    pivots.remove(pivots[i+1])
+                i = 0
+        i += 1
     return pivots
 
-'''def plot_candles_with_pivots(data, pivots, title='Candlestick with Pivots'):
-    data = data.copy()
-    data.index = pd.to_datetime(data.index)
-    fig = go.Figure(data=[go.Candlestick(
-        x=data.index,
-        open=data['Open'],
-        high=data['High'],
-        low=data['Low'],
-        close=data['Close'],
-        name='Candles')])
-    # Agregar los pivots
-    for pivot in pivots:
-        color = 'red' if pivot['type'] == 'high' else 'blue'
-        symbol = 'triangle-up' if pivot['type'] == 'high' else 'triangle-down'
-        fig.add_trace(go.Scatter(
-            x=[pivot['index']],
-            y=[pivot['value']],
-            mode='markers',
-            marker=dict(color=color, size=12, symbol=symbol),
-            name=f"Pivot {pivot['type']}"
-        ))
-    fig.update_layout(title=title, xaxis_title='Fecha', yaxis_title='Precio', xaxis_rangeslider_visible=True)
-    fig.show() '''
 
 def plot(data, ticker):
     mpf.plot(data, 
@@ -119,25 +81,6 @@ def plot(data, ticker):
     plt.show()
 
 def plot_candles_with_pivots(data, pivots, title='Candlestick with Pivots'):
-    apds = []
-    for pivot in pivots:
-        color = 'r' if pivot['type'] == 'high' else 'b'
-        marker = '^' if pivot['type'] == 'high' else 'v'
-        apds.append(
-            mpf.make_addplot(
-                [pivot['value'] if idx == pivot['index'] else None for idx in data.index],
-                type='scatter', markersize=100, marker=marker, color=color
-            )
-        )
-    mpf.plot(data, 
-             type='candle',
-            addplot=apds,
-            style='charles',
-            title='Candlestick with Pivots',
-            ylabel='Precio')
-    plt.show()
-
-def plot_candles_with_pivots_mpf(data, pivots, title='Candlestick with Pivots'):
     data = data.copy()
     data.index = pd.to_datetime(data.index)
     high_marker = [np.nan] * len(data)
