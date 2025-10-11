f4775965552a4a30a490d95a026a14288decf3b6
Luis Alberto Cortes Zambrano <luiscortes@DATECSA.LAN>
2025-10-11 12:41:55 -0500

    Inclusion test_pivots_graph

---
+++ rafa_main.py
@@ -0,0 +1,22 @@
+## Variables definition
+
+## 1 Make premarket analysis
+## 1.1 check if there are any assests with the three major timeframes aligned
+
+## 2. create watchlist of assets with aligned timeframes
+
+## 3. scan watchlist for entry signals
+
+## 4. if there are entry signals, check if create order conditions are met
+## 4.1 check if the market is open
+## 4.2 check if there is a related news event in the next 20 minutes (parametizable)
+## 4.3 check if there is an open position   
+## 4.4 check if RSI is not overbought/oversold
+
+
+## 5. if order conditions are met, create order
+## 5.1 set stop loss and take profit levels
+
+## manage open positions
+## 6.1 check if there are any open positions
+## 6.2 check if any open positions need to be closed
